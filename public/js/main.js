6a706759c4fb1cd8e9f1f3bee432b79af34f2239
Navneet Bahuguna <mr.navneet.bahuguna@gmail.com>
2021-07-08 23:38:23 +0530

    design changed

--- public/js/main.js
+++ public/js/main.js
@@ -65,7 +65,7 @@ function outputMessage(message) {
     const p = document.createElement('p');
     p.classList.add('meta');
     p.innerText = message.username;
-    p.innerHTML += `<span>${message.time}</span>`;
+    p.innerHTML += `<span style = "float: right">${message.time}</span>`;
     div.appendChild(p);
     const para = document.createElement('p');
     para.classList.add('text');
